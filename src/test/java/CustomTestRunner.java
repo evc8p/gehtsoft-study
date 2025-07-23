91f61479370bed825f40f0d1ffbdb3caa25175a2
evgenii <evch@rambler.ru>
2025-07-23 21:47:25 +0700

    Fixes

--- src/test/java/CustomTestRunner.java
+++ src/test/java/CustomTestRunner.java
@@ -2,10 +2,20 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Objects;
 
 public class CustomTestRunner {
+    int classesCounter = 0;
+    int totalTests = 0;
+    int passed = 0;
+    int failed = 0;
+    int skipped = 0;
+
     public static void main(String[] args) {
         CustomTestRunner customListTests = new CustomTestRunner();
         customListTests.runTests(CustomListTests.class, DemonstrationTests.class, PerformanceAndAnalysisTests.class);
@@ -16,11 +26,6 @@ public class CustomTestRunner {
         Method beforeEach;
         Method afterAll;
         Method afterEach;
-        int classesCounter = 0;
-        int totalTests = 0;
-        int passed = 0;
-        int failed = 0;
-        int skipped = 0;
         long executionTime = System.currentTimeMillis();
         for (Class<?> testClass : testClasses) {
             beforeAll = null;
@@ -59,7 +64,7 @@ public class CustomTestRunner {
                 }
 
                 // invoke the BeforeAll first
-                if (invokeMethod(beforeAll, testClassInstance) == -1) {
+                if (!invokeMethod(beforeAll, testClassInstance)) {
                     failed++;
                     break;
                 }
@@ -68,12 +73,12 @@ public class CustomTestRunner {
                     if (!method.isAnnotationPresent(Disabled.class)) {
                         if (method.isAnnotationPresent(Test.class) && !method.isAnnotationPresent(ParameterizedTest.class)) {
                             totalTests++;
-                            if (invokeMethod(beforeEach, testClassInstance) == -1) {
+                            if (!invokeMethod(beforeEach, testClassInstance)) {
                                 failed++;
                                 break;
                             }
-                            int i = invokeMethod(method, testClassInstance) == 0 ? passed++ : failed++;
-                            if (invokeMethod(afterEach, testClassInstance) == -1) {
+                            int i = invokeMethod(method, testClassInstance) ? passed++ : failed++;
+                            if (!invokeMethod(afterEach, testClassInstance)) {
                                 failed++;
                             }
                         }
@@ -81,135 +86,7 @@ public class CustomTestRunner {
                             System.out.println("--- Parameterized test started ---");
                             if (method.isAnnotationPresent(ValueSource.class)) {
                                 ValueSource valueSource = method.getAnnotation(ValueSource.class);
-                                for (short value : valueSource.shorts()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (byte value : valueSource.bytes()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (int value : valueSource.ints()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (long value : valueSource.longs()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (float value : valueSource.floats()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (double value : valueSource.doubles()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (char value : valueSource.chars()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (boolean value : valueSource.booleans()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (String value : valueSource.strings()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                }
-                                for (Class<?> value : valueSource.classes()) {
-                                    totalTests++;
-                                    if (invokeMethod(beforeEach, testClassInstance) == -1) {
-                                        failed++;
-                                        break;
-                                    }
-                                    System.out.print("With value: ");
-                                    int i = invokeMethod(method, testClassInstance, value) == 0 ? passed++ : failed++;
-                                    if (invokeMethod(afterEach, testClassInstance) == -1) {
-                                        failed++;
-                                    }
-                                }
+                                processVolumeSource(valueSource, method, testClassInstance, beforeEach, afterEach);
                             }
                             System.out.println("--- Parameterized test completed ---");
                         }
@@ -221,7 +98,7 @@ public class CustomTestRunner {
                 }
 
                 // invoke the AfterAll method last
-                if (invokeMethod(afterAll, testClassInstance) == -1) {
+                if (!invokeMethod(afterAll, testClassInstance)) {
                     failed++;
                     break;
                 }
@@ -245,19 +122,45 @@ public class CustomTestRunner {
         System.out.printf("Success rate\t%.1f %%\n", (100.0f * (float) passed / (float) totalTests));
     }
 
-    private int invokeMethod(Method method, Object obj, Object... args) {
+    private boolean invokeMethod(Method method, Object obj, Object... args) {
         if (method != null) {
             long time = System.currentTimeMillis();
             try {
                 Object object = args == null ? method.invoke(obj) : method.invoke(obj, args);
                 System.out.printf("✓ %s (%d ms)\n", method.getName(), (System.currentTimeMillis() - time));
-                return 0;
             } catch (Exception e) {
                 System.out.printf("✗ %s (%d ms)\n", method.getName(), (System.currentTimeMillis() - time));
                 e.printStackTrace(System.out);
-                return -1;
+                return false;
             }
         }
-        return 1;
+        return true;
+    }
+
+    private void processVolumeSource(ValueSource valueSource, Method method, Object testClassInstance, Method beforeEach, Method afterEach) throws InvocationTargetException, IllegalAccessException {
+        Method[] valueSourceMethods = ValueSource.class.getDeclaredMethods();
+        for (Method valueSourceMethod : valueSourceMethods) {
+            Object values = null;
+            values = valueSourceMethod.invoke(valueSource);
+            if (values != null && values.getClass().isArray()) {
+                int length = Array.getLength(values);
+                if (length > 0) {
+                    for (int i = 0; i < length; i++) {
+                        Object value = Array.get(values, i);
+                        totalTests++;
+                        if (!invokeMethod(beforeEach, testClassInstance)) {
+                            failed++;
+                            break;
+                        }
+                        System.out.print("With value: ");
+                        int s = invokeMethod(method, testClassInstance, value) ? passed++ : failed++;
+                        if (!invokeMethod(afterEach, testClassInstance)) {
+                            failed++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
